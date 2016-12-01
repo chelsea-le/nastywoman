3ed1e5ebd95a3a27b5f9dbe1e0e929e8e92367f7
Keertana Chandar <kchandar@uw.edu>
2016-12-01 10:03:19 -0800

    trump twitter feed api

--- src/TrumpTwitterFeed.js
+++ src/TrumpTwitterFeed.js
@@ -1,17 +1,28 @@
 // Tweets component
 import React from 'react';
 import './css/Tweet.css';
+import $ from 'jquery'
 import '../node_modules/font-awesome/css/font-awesome.css'
 
 var TrumpTwitterFeed = React.createClass({
-
-    render:function() {
+    getInitialState:function() {
+        return {tweets: []};
+    },
+    componentDidMount:function() {
+      $.get('https://faculty.washington.edu/joelross/proxy/twitter/timeline/?screen_name=realDonaldTrump&count=100')
+          .then(function(data) {
+          console.log(data)
+          this.setState({tweets: data.results});
+      }.bind(this));      
+    },
+    render:function() {   
+      console.log(this.state.tweets)
       return(
         <div>
-          <a class="twitter-timeline"
-            href="https://twitter.com/realDonaldTrump">
+          <a className="twitter-timeline" target='_blank' data-link-color="#E95F28" href="https://twitter.com/realDonaldTrump">
             Tweets by @realDonaldTrump
-          </a>
+          </a> 
+          <script async src="//platform.twitter.com/widgets.js" charSet="utf-8"></script>       
         </div>
       )
     }
