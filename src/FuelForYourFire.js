c95307cf13e7ef5b6c90d4bcadb0691f83d9fd47
Keertana Chandar <kchandar@uw.edu>
2016-12-03 18:13:50 -0800

    Trump feed and search tabs finished

--- src/FuelForYourFire.js
+++ src/FuelForYourFire.js
@@ -11,9 +11,20 @@ var FuelForYourFire = React.createClass({
     render:function() {   
       return (
         <div>
-          <SearchHashtags />
-
-          <TrumpTwitterFeed />
+          <div className="row">
+            <div className="col s12">
+              <ul className="tabs">
+                <li className="tab col s3"><a className="active" href="#test1">@realDonaldTrump</a></li>
+                <li className="tab col s3"><a href="#test2">#donaldtrump</a></li>
+              </ul>
+            </div>
+            <div id="test1" className="col s12">
+              <TrumpTwitterFeed />
+            </div>
+            <div id="test2" className="col s12">
+              <SearchHashtags />
+            </div>
+          </div>     
         </div>
       )
     }
--- src/SearchHashtags.js
+++ src/SearchHashtags.js
@@ -6,42 +6,27 @@ import 'materialize-css'
 import '../node_modules/font-awesome/css/font-awesome.css'
 
 var SearchHashtags = React.createClass({
-    getInitialState:function() {
-        return {hashtag:""}
-    },
-    setHashtagState:function(e) {
-      this.setState({hashtag:e.target.value})
-    },
-    buttonClick:function() {
-      var commentValue = this.state.comment
-      this.setState({comment:''})
-      {/*this.props.revealComments(commentValue)*/}
-    }, 
 	render:function() {
 		return (
-		<div className="search">
-			<div className="input-field col s3" id="hashtag-search">
-				<input id="search" type="text" value={this.state.hashtag} 
-				onChange={this.setHashtagState} placeholder='Search for a hashtag' className="validate"/>
-			</div>
-			<button type="submit" className="btn" id="broadcast-btn" onClick={this.buttonClick}>Search!</button>
-			<a className="twitter-timeline"  href="https://twitter.com/hashtag/TwitterStories" data-widget-id="804512822025560064">
-				#TwitterStories Tweets
-			</a>
+		<div className="hashtagFeed">
+			<h3>@realDonaldTrump Twitter Feed</h3>
+            <a className="twitter-timeline" target="_blank" href="https://twitter.com/hashtag/donaldtrump" data-widget-id="805227545452871680">
+            	#donaldtrump Tweets
+        	</a>
 			<script>
-				{!function(d,s,id) {
+				{!function(d,s,id){
 					var js
 					var fjs=d.getElementsByTagName(s)[0]
 					var p=/^http:/.test(d.location)?'http':'https'
 					if(!d.getElementById(id)) {
-						js=d.createElement(s)
+						js = d.createElement(s)
 						js.id=id
 						js.src=p+"://platform.twitter.com/widgets.js"
 						fjs.parentNode.insertBefore(js,fjs)
 					}
 				}
 				(document,"script","twitter-wjs")}
-			</script>                       
+			</script>                     
 		</div>
 	)}
 });
